4b770a544e6e3f4596724d901620dca33ba2e823
feng.lin098 <feng.lin098@gmail.com>
2019-10-26 08:05:04 -0400

    final

--- src/E5.java
+++ src/E5.java
@@ -6,22 +6,16 @@ public class E5 {
     public static void main(String[] args){
         Scanner keyboard=new Scanner(System.in);
         System.out.println("Please enter ten numbers:");
-        int userInput=keyboard.nextInt();
-        int[] numArray=new int[10];
         ArrayList<Integer> oddArr= new ArrayList<Integer>();
         ArrayList<Integer> evenArr= new ArrayList<Integer>();
         for (int i=0;i<10;i++){
-            numArray[i]=userInput;
-            if (numArray[i]%2==0){
-                evenArr.add(numArray[i]);
+            int userInput=keyboard.nextInt();
+            if (userInput%2==0){
+                evenArr.add(userInput);
             }else {
-                oddArr.add(numArray[i]);
-            }for (int e:evenArr) {
-                System.out.println("The even numbers are: "+e);
-            }for (int o:oddArr){
-                System.out.println("The odd numbers are: "+o);
+                oddArr.add(userInput);
             }
-        }
-
+        }System.out.print("The even numbers are: "+evenArr+"\n");
+        System.out.print("The odd numbers are: "+oddArr);
     }
 }
